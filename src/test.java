dd07d949a1ed08ff0b8534841474a7d81ede39b3
Adam <adam_s1308@yahoo.com>
2021-02-27 23:27:08 +0800

    aku tambah sikit je

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("iMEssage apa");
         System.out.println("Adam u gud lol");
+        System.out.println("Thanks bro");
     }
 }
