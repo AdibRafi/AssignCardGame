3babb10da3f88c5b2b36eb8859264cd76b7a3656
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-02-28 23:05:41 +0800

    .

--- src/Function/Card.java
+++ src/Function/Card.java
@@ -1,13 +1,12 @@
 package Function;
 
 public class Card {
-    public static String[] shuffleCards(String[] cards){
+    public static void shuffleCards(String[] cards){
         for (int i = 0; i < cards.length; i++) {
             int r = i + (int) (Math.random() * (cards.length-i));
             String temp = cards[r];
             cards[r] = cards[i];
             cards[i] = temp;
         }
-        return cards;
     }
 }
