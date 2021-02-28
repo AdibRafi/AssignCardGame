077e79d69f04d19aab7cf9ba7635db9e7b41421e
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-02-28 22:35:48 +0800

    adding shuffle in Card

--- src/Function/Card.java
+++ src/Function/Card.java
@@ -1,4 +1,13 @@
 package Function;
 
 public class Card {
+    public static String[] shuffleCards(String[] cards){
+        for (int i = 0; i < cards.length; i++) {
+            int r = i + (int) (Math.random() * (cards.length-i));
+            String temp = cards[r];
+            cards[r] = cards[i];
+            cards[i] = temp;
+        }
+        return cards;
+    }
 }
