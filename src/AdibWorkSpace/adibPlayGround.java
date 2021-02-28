274689a9fdbb4e9711e11bf3838a069eb0a4cc3d
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-02-28 23:05:47 +0800

    .

--- src/AdibWorkSpace/adibPlayGround.java
+++ src/AdibWorkSpace/adibPlayGround.java
@@ -1,8 +1,14 @@
 package AdibWorkSpace;
 
+// import benda dlm package Function
+import Function.Card;
+import Function.Player;
+
+import java.util.Arrays;
+
 public class adibPlayGround {
     public static void main(String[] args){
-        adibDeckOfCards.start();
-
+        String[] test = {"c2","c3","d8","d5","sK"};
+        Card.shuffleCards(test);
     }
 }
