7c364ae47fc9b56dcb57e47bee37fc1c2b1def01
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-02-28 22:56:29 +0800

    letak some basic stuff in Player.java

--- src/Function/Player.java
+++ src/Function/Player.java
@@ -1,4 +1,21 @@
 package Function;
 
 public class Player {
+    int playerScore = 0;
+
+    public Player(){}
+
+    public void restartScore(){
+        playerScore = 0;
+    }
+    public void addValue(int value){
+        playerScore += value;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "playerScore=" + playerScore +
+                '}';
+    }
 }
