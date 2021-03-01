d1f67e8a1a0d1456f438fa2ff928b8a6b1d553f9
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-03-01 17:12:40 +0800

    letak playerCards dlm Game

--- src/Function/Game.java
+++ src/Function/Game.java
@@ -1,4 +1,19 @@
 package Function;
 
-public class Game {
+import java.util.Set;
+
+public class Game{
+    Set<String> playerCards;
+    public Game(){}
+    public Game(Set<String> playerCards){
+        this.playerCards = playerCards;
+    }
+    public void removeCardsFromPlayers
+            (Set<String> original, Set<String> remove){
+        original.removeAll(remove);
+    }
+
+    public Set<String> PlayerCards(){
+        return playerCards;
+    }
 }
