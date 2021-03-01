9259e1fa34a595d96e908c83c73011cf5ec0617c
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-03-01 17:12:05 +0800

    .

--- src/AdibWorkSpace/adibPlayGround.java
+++ src/AdibWorkSpace/adibPlayGround.java
@@ -2,13 +2,24 @@ package AdibWorkSpace;
 
 // import benda dlm package Function
 import Function.Card;
+import Function.Game;
 import Function.Player;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class adibPlayGround {
-    public static void main(String[] args){
-        String[] test = {"c2","c3","d8","d5","sK"};
-        Card.shuffleCards(test);
+    public static void main(String[] args) {
+        //todo tukar the main array into set<String>
+
+        //todo from array adam of all cards -> masuk dlm playerCards
+        // in Game (kena guna Set<String>)
+
+        String[] test = {"c2","c3","d4","dA","sK","cA"};
+        String[] deleteTest = {"c3","dA","sK"};
+        Set<String> s = new TreeSet<>(Arrays.asList(test));
+        Set<String> d = new HashSet<>(Arrays.asList(deleteTest));
+        Player p1 = new Player("adib",s);
+        p1.removeCardsFromPlayers(p1.PlayerCards(),d);
+        System.out.println(p1);
     }
 }
