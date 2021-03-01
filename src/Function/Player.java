dd8c1b2584943001b1162c4a89ec5eff0ff9c94f
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-03-01 17:12:59 +0800

    Inheritance Player extends Game

--- src/Function/Player.java
+++ src/Function/Player.java
@@ -1,13 +1,21 @@
 package Function;
 
-public class Player {
+import java.util.Set;
+
+public class Player extends Game{
     int playerScore = 0;
     String playerName;
 
-    public Player(){}
+    public Player(){
+    }
     public Player(String name){
         this.playerName = name;
     }
+    public Player(String name, Set<String> cards){
+        super(cards);
+        this.playerName = name;
+    }
+
 
     public void restartScore(){
         playerScore = 0;
@@ -19,7 +27,7 @@ public class Player {
     @Override
     public String toString() {
         return "Player{" +
-                "playerScore=" + playerScore +
+                "playerCards=" + playerCards +
                 '}';
     }
 }
