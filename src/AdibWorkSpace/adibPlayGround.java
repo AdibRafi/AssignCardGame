10be0d0824b58f60a2af6214719a6d1b012c292a
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-03-02 12:51:12 +0800

    .

--- src/AdibWorkSpace/adibPlayGround.java
+++ src/AdibWorkSpace/adibPlayGround.java
@@ -9,17 +9,42 @@ import java.util.*;
 
 public class adibPlayGround {
     public static void main(String[] args) {
-        //todo tukar the main array into set<String>
+        Player p1 = new Player("adib");
+        Player p2 = new Player("adam");
+        Player p3 = new Player("darwisy");
 
-        //todo from array adam of all cards -> masuk dlm playerCards
-        // in Game (kena guna Set<String>)
+        String[] cards1 = {"c5","s6","sK","dA","cK","h5","h3","dJ","d8","s7","cX","c2","h4","hA","d2","hJ","hX","s2"};
+        String[] cards2 = {"d4","h7","c4","cQ","sA","d5","s3","d3","h2","h8","c9","hK","d6","sJ","sX","s8","d7"};
+        String[] cards3 = {"cA","dX","h6","dQ","d9","c8","h9","hQ","sQ","cJ","dK","c6","s9","s4","c7","s5","c3"};
+        String[] fiveElement1 = {"c5","s6","sK","dA","cK"};
+        String[] fiveElement2 = {"d4","h7","c4","cQ","sA"};
+        String[] fiveElement3 = {"cA","dX","h6","dQ","d9"};
 
-        String[] test = {"c2","c3","d4","dA","sK","cA"};
-        String[] deleteTest = {"c3","dA","sK"};
-        Set<String> s = new TreeSet<>(Arrays.asList(test));
-        Set<String> d = new HashSet<>(Arrays.asList(deleteTest));
-        Player p1 = new Player("adib",s);
-        p1.removeCardsFromPlayers(p1.PlayerCards(),d);
-        System.out.println(p1);
+
+
+
+
+
+
+
+
+
+// ARCHIVE
+//        String[] test = {"c2","c3","d4","dA","sK","cA"};
+//        String[] deleteTest = {"c3","dA","sK"};
+//        Set<String> x = new HashSet<>(Arrays.asList(test));
+//        Set<String> y = new HashSet<>(Arrays.asList(deleteTest));
+//        Queue<String> a = new LinkedList<>(Arrays.asList(test));
+//        Queue<String> b = new LinkedList<>(Arrays.asList(deleteTest));
+//        System.out.println(x);
+//        x = Card.shuffleSet(x);
+//        System.out.println(x);
+
+
+
+//        System.out.println(a);
+//        a = Card.shuffleQueue(a);
+//        System.out.println(a);
+
     }
 }
