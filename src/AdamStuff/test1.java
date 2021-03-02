0c3b294cc31d74113f00f6638cca9fcf220c5a1d
Adam <adam_s1308@yahoo.com>
2021-03-03 00:14:49 +0800

    Returned 5 elements into an array and sort it out.

--- src/AdamStuff/test1.java
+++ src/AdamStuff/test1.java
@@ -1,12 +1,13 @@
 package AdamStuff;
 
+import java.util.Comparator;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import Function.Player;
 import Function.Card;
 
-public class test1 {
+public class test1{
     public static void main(String[] args) {
 
         //3 Player Phase
@@ -70,8 +71,65 @@ public class test1 {
         System.out.println(P4.displaySet());
         System.out.println(P5.displaySet());
 
-        String[] testje = {"sK","sX","d5","c4","s9"};
-        int result = Card.getValueFromCard(testje);
-        System.out.println(result);
+        String[] test1 = P1.return5();
+        System.out.println(Arrays.toString(test1));
+
+        String[] test2 = P2.return5();
+        System.out.println(Arrays.toString(test2));
+
+        String[] test3 = P3.return5();
+        System.out.println(Arrays.toString(test3));
+
+//        String[] testje = {"sK","sX","d5","c4","s9"};
+//        int result = Card.getValueFromCard(testje);
+//        System.out.println(result);
+
+//        String[] newHandP1 = new String[5];
+//        String[] newHandP2 = new String[5];
+//        String[] newHandP3 = new String[5];
+
+//        for (int i = 0; i < 5; i++) {
+//            newHandP1[i] = P1.getPlayerCardsQueue().remove();
+//        }
+//        Arrays.sort(newHandP1, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.compareTo(o2);
+//            }
+//        });
+//
+//        String[] first5ElementP1 = new String[5];
+//        for(int i = 0; i < 5; i++){
+//            first5ElementP1[i] = newHandP1[i];
+//        }
+//        System.out.println(Arrays.toString(first5ElementP1));
+
+//        for (int i = 0; i < 5; i++) {
+//            newHandP2[i] = P2.getPlayerCardsQueue().remove();
+//        }
+//        Arrays.sort(newHandP2, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.compareTo(o2);
+//            }
+//        });
+//
+//        for(String str:newHandP2){
+//            System.out.println(str);
+//        }
+//
+//        for (int i = 0; i < 5; i++) {
+//            newHandP3[i] = P3.getPlayerCardsQueue().remove();
+//        }
+//        Arrays.sort(newHandP3, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.compareTo(o2);
+//            }
+//        });
+//
+//        for(String str:newHandP3){
+//            System.out.println(str);
+//        }
     }
 }
