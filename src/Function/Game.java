4aa838222791776335e5bf03795dee7ad3f8eb0c
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-03-02 11:48:19 +0800

    letak the shuffling mode for each player dlm bentuk Queue and Set

--- src/Function/Game.java
+++ src/Function/Game.java
@@ -1,19 +1,46 @@
 package Function;
 
-import java.util.Set;
+import java.util.*;
 
 public class Game{
-    Set<String> playerCards;
+    Queue<String> playerCardsQueue = new LinkedList<>();
+    Set<String> playerCardsSet = new LinkedHashSet<>();
+
     public Game(){}
-    public Game(Set<String> playerCards){
-        this.playerCards = playerCards;
+    // Distribute cards
+    public void distributeCardsQueue(String[] cards){
+        playerCardsQueue.addAll(Arrays.asList(cards));
     }
-    public void removeCardsFromPlayers
-            (Set<String> original, Set<String> remove){
-        original.removeAll(remove);
+    public void distributeCardsSet(String[] cards){
+        playerCardsSet.addAll(Arrays.asList(cards));
+    }
+    //remove cards from ori
+    public void removeCardsQueue(Queue<String> remove){
+        int loop = remove.size();
+        for (int i = 0; i < loop; i++) {
+            playerCardsQueue.remove();
+        }
+    }
+    public void removeCardsSet(Set<String> remove){
+        playerCardsSet.removeAll(remove);
+    }
+    // Shuffling a cards from player
+    public void shuffleQueue(){
+        this.playerCardsQueue = Card.shuffleQueue(playerCardsQueue);
+    }
+    public void shuffleSet(){
+        this.playerCardsSet = Card.shuffleSet(playerCardsSet);
     }
 
-    public Set<String> PlayerCards(){
-        return playerCards;
+    // TESTING DISPLAY ONLY
+    public String displayQueue() {
+        return "Game{" +
+                "playerCardsQueue=" + playerCardsQueue +
+                '}';
+    }
+    public String displaySet() {
+        return "Game{" +
+                "playerCardsSet=" + playerCardsSet +
+                '}';
     }
 }
