d7ab0422f16126fadd76d05388a84fd5e286b544
Abdjb <63862086+Abdjb@users.noreply.github.com>
2021-03-04 19:03:15 +0800

    .

--- src/AdibWorkSpace/adibPlayGround.java
+++ src/AdibWorkSpace/adibPlayGround.java
@@ -7,6 +7,7 @@ import Function.Player;
 
 import java.util.*;
 
+
 public class adibPlayGround {
     public static void main(String[] args) {
         Player p1 = new Player("adib");
@@ -20,14 +21,63 @@ public class adibPlayGround {
         String[] fiveElement2 = {"d4","h7","c4","cQ","sA"};
         String[] fiveElement3 = {"cA","dX","h6","dQ","d9"};
 
+        int index = 0;
+        int[] test = {12,3,4};
+        int min = test[0];
+
+        for (int i = 0; i < 3; i++) {
+            if (test[i] < min) {
+                min = test[i];
+                index=i;
+            }
+        }
+        System.out.println(min);
+        System.out.println(index);
+
+//        Set<String> x = new LinkedHashSet<>(Arrays.asList(fiveElement1));
+//        String[] nice = new String[5];
+//        for (int i = 0; i < 5; i++) {
+//            nice[i] = x.iterator().next();
+//            x.remove(x.iterator().next());
+//        }
+//        System.out.println(Arrays.toString(nice));
+//        System.out.println(x);
 
 
 
 
 
 
+//        String[] testje = getDeckOfCards();
+//        Queue<String> l = new LinkedList<>(Arrays.asList(testje));
+//        String[] cardsToGiveForP1 = new String[18];
+//        String[] cardsToGiveForP2 = new String[17];
+//        String[] cardsToGiveForP3 = new String[17];
+//
+//        for (int i = 0; i < 18; i++) {
+//            cardsToGiveForP1[i] = l.remove();
+//        }
+//        for (int i = 0; i < 17; i++) {
+//            cardsToGiveForP2[i] = l.remove();
+//        }
+//        for (int i = 0; i < 17; i++) {
+//            cardsToGiveForP3[i] = l.remove();
+//        }
+//        p1.distributeCardsQueue(cardsToGiveForP1);
+//        p1.distributeCardsQueue(cardsToGiveForP2);
+//        p1.distributeCardsQueue(cardsToGiveForP3);
 
-
+//        p1.distributeCardsQueue(cards1);
+//        p1.shuffleQueue();
+//        System.out.println(p1.displayQueue());
+//        p1.removeCardsQueue();
+//        System.out.println(p1.displayQueue());
+//
+//        p2.distributeCardsSet(cards2);
+//        p2.shuffleSet();
+//        System.out.println(p2.displaySet());
+//        p2.removeCardsSet(fiveElement2);
+//        System.out.println(p2.displaySet());
 
 // ARCHIVE
 //        String[] test = {"c2","c3","d4","dA","sK","cA"};
@@ -47,4 +97,23 @@ public class adibPlayGround {
 //        System.out.println(a);
 
     }
+    private static String[] getDeckOfCards() {
+        ArrayList<String> deckOfCards = new ArrayList<>();
+
+        String[] suits = {"d", "c", "h", "s"};
+        String[] ranks = {"K", "Q", "J", "X", "9", "8", "7", "6", "5", "4", "3", "2", "A"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deckOfCards.add(suit + rank);
+            }
+        }
+        String[] result = new String[deckOfCards.size()];
+        for (int i = 0; i < deckOfCards.size(); i++) {
+            result[i] = deckOfCards.get(i);
+        }
+        return result;
+    }
 }
+
+
